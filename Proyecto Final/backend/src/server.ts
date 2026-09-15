7a7b4419a84931ebd64fcd7ef8740cd90a76c768
DagoFT <diego.alejandro.gomez.alcazar@gmail.com>
2026-09-14 20:19:49 -0400

    .get

--- Proyecto Final/backend/src/server.ts
+++ Proyecto Final/backend/src/server.ts
@@ -400,10 +400,8 @@ const frontendPath = path.resolve(
 
 app.use(express.static(frontendPath));
 
-app.get("*", (_request, response) => {
-    response.sendFile(
-        path.join(frontendPath, "index.html"),
-    );
+app.get("/{*splat}", (_request, response) => {
+    response.sendFile(path.join(frontendPath, "index.html"));
 });
 
 app.listen(PORT, () => {
